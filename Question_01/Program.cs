dd1e7662fe5f05ea9e6307ea848d4fa4f8f89ab8
Arhises <roma1039@gmail.com>
2023-02-11 21:34:21 +0200

    First question added

---
+++ Question_01/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+void Print(int[] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+int[] FillBenary(int n)
+{
+    int[] binnary = new int[n];
+    Random ran = new Random();
+    for (int i = 0; i < binnary.GetLength(0); i++)
+    {
+        binnary[i] = ran.Next(0,2);// fill 0 or 1
+    }
+    return binnary;
+}
+int[] array = FillBenary(8);
+Print(array);

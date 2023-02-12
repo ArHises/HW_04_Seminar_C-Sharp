ccbe6a9a4642f185ff453daa31c1b8b5b69b23f9
Arhises <roma1039@gmail.com>
2023-02-12 19:59:10 +0200

    Third Question added

---
+++ Question_03/Program.cs
@@ -0,0 +1,32 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+int[] FillThreeDigit(int n)
+{
+    int[] array = new int[n];
+    Random ran = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        array[i] = ran.Next(0,200);// fill 100 to 199
+    }
+    return array;
+}
+void Print(int[] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+int FindTenToNinetynine(int[] array){
+    int counter = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] >= 10 && array[i] < 100) counter++;
+    }
+    return counter;
+}
+int[] array = FillThreeDigit(123);
+Print(array);
+Console.WriteLine($"Number of numbers between 10 and 100 is: {FindTenToNinetynine(array)}");
+

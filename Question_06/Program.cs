3b15a64686d0260a84e15bf47662d19027803b85
Arhises <roma1039@gmail.com>
2023-02-12 21:04:45 +0200

    Sixth question added

---
+++ Question_06/Program.cs
@@ -0,0 +1,43 @@
+﻿// В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+
+double[] FillThreeDigit(int n)
+{
+    double[] array = new double[n];
+    Random ran = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        array[i] = ran.Next(0,50);// fill 0 to 50
+    }
+    return array;
+}
+void Print(double[] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write($"{array[i]} | ");
+    }
+    Console.WriteLine();
+}
+double FindMax(double[] array){
+    double max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > max) max = array[i];
+    }
+    return max;
+}
+double FindMin(double[] array){
+    double min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] < min) min = array[i];
+    }
+    return min;
+}
+double Difference(double max, double min) => max - min;
+
+Console.WriteLine("Enter the length of the array: ");
+int length = int.Parse(Console.ReadLine());
+double[] array = FillThreeDigit(length);
+Print(array);
+Console.WriteLine($"{FindMax(array)} - {FindMin(array)} = {Difference(FindMax(array),FindMin(array))}");
